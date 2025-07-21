d5e3a10f48653029a67143d64b1530b2e136127f
OIITuMuCT <vataman@live.ru>
2025-07-21 21:05:09 +0300

    Creating the user and superuser

--- core/user/models.py
+++ core/user/models.py
@@ -9,6 +9,44 @@ class UserManager(BaseUserManager):
         try:
             instance = self.get(public_id=public_id)
             return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+    def create_user(self, username, email, password=None, **kwargs):
+        """
+        Create and return a `User` with an email, phone 
+            number, username and password 
+        """
+        if username is None:
+            raise TypeError('Users must have a username.')
+        if email is None:
+            raise TypeError('Users must have an email.')
+        if password is None:
+            raise TypeError('User must have a password.')
+        
+        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    def create_superuser(self, username, email, password, **kwargs):
+        """
+        Create and return a `User` with superuser (admin) 
+            permissions. 
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+        if email is None:
+            raise TypeError('Superusers must have an email.')
+        if username is None:
+            raise TypeError('Superusers must have a username')
+        
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(db_index=True, unique=True, 
