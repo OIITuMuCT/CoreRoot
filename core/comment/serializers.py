b3152caa78115d3220e7e865804459d33430a315
OIITuMuCT <vataman@live.ru>
2025-07-25 11:50:01 +0300

    Writing the CommentViewSet class

--- core/comment/serializers.py
+++ core/comment/serializers.py
@@ -10,23 +10,39 @@ from core.post.models import Post
 
 class CommentSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(
-        queryset=User.objects.all(), slug_field='public_id'
+        queryset=User.objects.all(), slug_field="public_id"
     )
     post = serializers.SlugRelatedField(
-        quryset=Post.objects.all(), slug_field='public_id'
+        queryset=Post.objects.all(), slug_field="public_id"
     )
+
+    def validate_author(self, value):
+        if self.context["request"].user != value:
+            raise ValidationError("You can't create a post for another user.")
+        return value
+
+    def validate_post(self, value):
+        if self.instance:
+            return self.instance.post
+        return value
+
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data["edited"] = True
+
+        instance = super().update(instance, validated_data)
+
+        return instance
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
-        author = User.objects.get_objects_by_public_id(rep['author'])
-        rep['author'] = UserSerializer(author).data
+        author = User.objects.get_object_by_public_id(rep["author"])
+        rep["author"] = UserSerializer(author).data
 
         return rep
-    
+
     class Meta:
         model = Comment
-        # List of all the fields that can be included in a 
-        # request or a response
-        fields = ['id', 'post', 'author', 'body', 'edited', 'created', 'updated']
-        read_only = ["edited"]
-
+        # List of all the fields that can be included in a request or a response
+        fields = ["id", "post", "author", "body", "edited", "created", "updated"]
+        read_only_fields = ["edited"]
