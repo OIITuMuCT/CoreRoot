7db80a53776982e3b901716bef9137c707ffff2d
OIITuMuCT <vataman@live.ru>
2025-07-29 16:07:09 +0300

    Writing the request service

--- social-media-app/src/helpers/axios.js
+++ social-media-app/src/helpers/axios.js
@@ -3,19 +3,56 @@ import axios from "axios";
 import createAuthRefreshInterceptor from "axios-auth-refresh";
 
 const axiosService = axios.create({
-    baseURL: "http://localhost:8000",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 axiosService.interceptors.request.use(async (config) => {
-    /**
-     * Retrieving the access token from the local Storage and adding it to the headers of the request
-     */
-    const { access } =
-        JSON.parse(localStorage.getItem("auth"));
-    config.headers.Authorization = `Bearer ${access}`;
-    return config;
+  /**
+   * Retrieving the access token from the local Storage and adding it to the headers of the request
+   */
+  const { access } = JSON.parse(localStorage.getItem("auth"));
+  config.headers.Authorization = `Bearer ${access}`;
+  return config;
 });
 
+axiosService.interceptors.response.use(
+  (res) => Promise.resolve(res),
+  (err) => Promise.reject(err)
+);
+
+const refresAuthLogic = async (failedRequest) => {
+  const { refresh } = JSON.parse(localStorage.getItem("auth"));
+  return axios
+    .post("/refresh/token/", null, {
+      baseURL: "http://localhost:8000",
+      headers: {
+        Authorization: `Bearer ${refresh}`,
+      },
+    })
+    .then((resp) => {
+      const { access, refresh } = resp.data;
+      failedRequest.response.config.headers["Authorization"] =
+        "Bearer " + access;
+      localStorage.setItem(
+        "auth",
+        JSON.stringify({
+          access,
+          refresh,
+        })
+      );
+    })
+    .catch(() => {
+      localStorage.removeItem("auth");
+    });
+};
+
+createAuthRefreshInterceptor(axiosService, refresAuthLogic);
+
+export function fetcher(url) {
+  return axiosService.get(url).then((res) => res.data);
+}
+
+export default axiosService;
