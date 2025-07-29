449cf9a9d34d257edbd5b0501fe002afdf980d3a
OIITuMuCT <vataman@live.ru>
2025-07-29 19:37:26 +0300

    Adding ProdectedRoute

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Registration from "./pages/Registration";
 import Home from "./pages/Home";
 import Counter from './pages/Counter';
 import Form from "./pages/handling-form";
@@ -9,7 +11,16 @@ import Form2 from "./pages/form2";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login/" element={<div>Login</div>} />
+      <Route path="/register/" element={<Registration />} />
       <Route path="/count" element={<Counter />} />
       <Route path="/form" element={<Form />} />
       <Route path="/form2" element={<Form2 />} />
